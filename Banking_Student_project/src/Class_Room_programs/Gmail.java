435d4f3ebed028582f0135d2ce9b9bf3c9745ceb
Ritish <Ritish@DESKTOP-UC7P1MU>
2023-02-09 14:13:51 +0530

    r execicution

---
+++ Banking_Student_project/src/Class_Room_programs/Gmail.java
@@ -0,0 +1,21 @@
+package Class_Room_programs;
+
+public class Gmail {
+	
+	public webdriver driver = new webdriver;
+	
+
+	public static void main(String[] args) {
+	} 
+		driver =  new chromedriver();
+		{
+		}
+		public void openurl();
+}
+		
+		public void login();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Banking_Student_project/src/Class_Room_programs/Google.java
@@ -0,0 +1,13 @@
+package Class_Room_programs;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+   
+	
+	
+	
+}
